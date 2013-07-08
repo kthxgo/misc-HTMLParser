c1447915fe9f3ca107dd346b93c520a528396f69
Jeff <Jeff's Mail>
2013-07-08 23:14:16 +0200

    ...

--- code/common/Config.java
+++ code/common/Config.java
@@ -3,7 +3,6 @@ package common;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,80 +13,80 @@ import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
-import common.Config.Keys_Defs.DoubleTypes;
+import common.Config.Keys_Defs.BooleanTypes;
 import common.Config.Keys_Defs.IntTypes;
 import common.Config.Keys_Defs.StringTypes;
 
-
-/**
+/** Class used to store Runtime relavant Values which should be editable via an
+ * external XML-File
  * 
- * @author S. Berndt
- *
- */
+ * @author S. Berndt */
 public class Config {
 
-	private static final String	INT_TYPES			= "intTypes";
-	private static final String	DOUBLE_TYPES		= "doubleTypes";
-	private static final String	STRING_TYPES		= "stringTypes";
+	private static final String	INT_TYPES				= "intTypes";
+	private static final String	BOOL_TYPES				= "boolTypes";
+	private static final String	STRING_TYPES			= "stringTypes";
 
-	private static final File	CONFIGFILE_LOCATION	= new File("." + File.separator + "config" + File.separator +  "config.xml");
-	public static final File	LOG_CONFIGFILE_LOCATION	= new File("." + File.separator + "config" + File.separator +  "logging.properties");
-	
-	private static final Config singleton = new Config();
-	
-	private Logger log;
-	
-	private Config(){
+	private static final File	CONFIGFILE_LOCATION		= new File("." + File.separator + "config" + File.separator + "config.xml");
+	public static final File	LOG_CONFIGFILE_LOCATION	= new File("." + File.separator + "config" + File.separator + "logging.properties");
+
+	private static final Config	singleton				= new Config();
+
+	private Logger				log;
+
+	private Config() {
 		log = Logger.getLogger(this.getClass().getName());
-		log.setLevel(Level.FINEST);
+		log.setLevel(Level.ALL);
 	}
-	
-	
-	public static Config getInstance(){
+
+	public static Config getInstance() {
 		return singleton;
 	}
 
+	/** trys to load the Values from the XML-File. If there are Problems, e.g. a
+	 * Value-Set is missing or the Value is unparsable the specified default Key
+	 * is keept. */
 	public void loadXML() {
 		if (CONFIGFILE_LOCATION.exists()) {
 			log.fine("Config File existing, try to load values...");
 			try {
 				Element root = new SAXBuilder().build(CONFIGFILE_LOCATION).getRootElement();
-				Element ints = root.getChild(INT_TYPES,root.getNamespace());
-				Element doubles = root.getChild(DOUBLE_TYPES,root.getNamespace());
-				Element strings = root.getChild(STRING_TYPES,root.getNamespace());
-				for (IntTypes key : Keys_Defs.IntTypes.values()){
+				Element ints = root.getChild(INT_TYPES, root.getNamespace());
+				Element doubles = root.getChild(BOOL_TYPES, root.getNamespace());
+				Element strings = root.getChild(STRING_TYPES, root.getNamespace());
+				for (IntTypes key : Keys_Defs.IntTypes.values()) {
 					try {
 						Element valueSet = ints.getChild(key.name(), ints.getNamespace());
 						int value = Integer.parseInt(valueSet.getText());
 						key.setKey(value);
-						log.finest("loaded " +  key.name() + " = " + value);
+						log.finest("loaded " + key.name() + " = " + value);
 					} catch (NullPointerException e) {
-//						e.printStackTrace();
+						log.finest(key.name() + " missing, will use default Value");
 					} catch (NumberFormatException e) {
 						e.printStackTrace();
-					} 
+					}
 				}
-				for (DoubleTypes key : Keys_Defs.DoubleTypes.values()){
+				for (BooleanTypes key : Keys_Defs.BooleanTypes.values()) {
 					try {
 						Element valueSet = doubles.getChild(key.name(), doubles.getNamespace());
-						double value = Double.parseDouble(valueSet.getText());
+						boolean value = Boolean.parseBoolean(valueSet.getText());
 						key.setKey(value);
-						log.finest("loaded " +  key.name() + " = " + value);
+						log.finest("loaded " + key.name() + " = " + value);
 					} catch (NullPointerException e) {
-//						e.printStackTrace();
+						log.finest(key.name() + " missing, will use default Value");
 					} catch (NumberFormatException e) {
 						e.printStackTrace();
-					} 
+					}
 				}
-				for (StringTypes key : Keys_Defs.StringTypes.values()){
+				for (StringTypes key : Keys_Defs.StringTypes.values()) {
 					try {
 						Element valueSet = strings.getChild(key.name(), strings.getNamespace());
 						String value = valueSet.getText();
 						key.setKey(value);
-						log.finest("loaded " +  key.name() + " = " + value);
+						log.finest("loaded " + key.name() + " = " + value);
 					} catch (NullPointerException e) {
-//						e.printStackTrace();
-					}			
+						log.finest(key.name() + " missing, will use default Value");
+					}
 				}
 			} catch (JDOMException e) {
 				e.printStackTrace();
@@ -99,26 +98,31 @@ public class Config {
 		}
 	}
 
-	public void saveXML(){
+	/** trys to store all current Value-Sets into the XML-File. The File will be
+	 * rewritten no matter what was in it. Unused Entries will be deleted within
+	 * the XML-File. The Path to the XML-File is absolute specified within this
+	 * Class as <code>CONFIGFILE_LOCATION</code>. If the Directory pointing to
+	 * the XML-File does not exist the needed Structure will be generated */
+	public void saveXML() {
 		log.fine("Save Config to File");
 		Element root = new Element("config");
 		Element ints = new Element(INT_TYPES);
-		Element doubles = new Element(DOUBLE_TYPES);
+		Element bools = new Element(BOOL_TYPES);
 		Element strings = new Element(STRING_TYPES);
-		for (IntTypes key : Keys_Defs.IntTypes.values()){
+		for (IntTypes key : Keys_Defs.IntTypes.values()) {
 			ints.addContent(new Element(key.name()).setText(key.getKey() + ""));
 			log.finest("saved " + key.name() + "(" + key.getKey() + ")");
 		}
-		for (DoubleTypes key : Keys_Defs.DoubleTypes.values()){
-			doubles.addContent(new Element(key.name()).setText(key.getKey() + ""));
-			log.finest("saved " + key.name() + "(" + key.getKey() + ")");			
+		for (BooleanTypes key : Keys_Defs.BooleanTypes.values()) {
+			bools.addContent(new Element(key.name()).setText(key.getKey() + ""));
+			log.finest("saved " + key.name() + "(" + key.getKey() + ")");
 		}
-		for (StringTypes key : Keys_Defs.StringTypes.values()){
-			strings.addContent(new Element(key.name()).setText(key.getKey() + ""));		
-			log.finest("saved " + key.name() + "(" + key.getKey() + ")");	
+		for (StringTypes key : Keys_Defs.StringTypes.values()) {
+			strings.addContent(new Element(key.name()).setText(key.getKey() + ""));
+			log.finest("saved " + key.name() + "(" + key.getKey() + ")");
 		}
 		root.addContent(ints);
-		root.addContent(doubles);
+		root.addContent(bools);
 		root.addContent(strings);
 		try {
 			if (!CONFIGFILE_LOCATION.getParentFile().exists()) {
@@ -138,19 +142,27 @@ public class Config {
 		public static enum IntTypes {
 			JUnit_TestValue(42),
 
-			CrawlerSupervisor_ThreadPoolCore(2), 
+			DataBase_Port(3306),
+
+			Analyzer_Shutdown_Timeout(2000),
+
 			CrawlerSupervisor_ThreadPoolMax(8),
-			CrawlerSupervisor_MaximumCrawlingDepth(32),
+			CrawlerSupervisor_MaximumCrawlingDepth(4),
 			Crawler_MaxSiteSize(25_000_000),
-			
-			CrawlerSupervisor_MinTimeDiff(1000*60*60*14),
-			
+
+			CrawlerSupervisor_MinTimeDiff(1000 * 60 * 60 * 14),
+			CrawlerSupervisor_ShutdownTimeout(5),
+
+			CrawlerSupervisor_CrawlingRequestQueueSize(50),
+			CrawlerOutputQueue_MaxSize(50),
+			IngestionInputQueue_MaxSize(50),
+
 			Plugin_Length(5000),
-			
+
 			Ingestion_msPerDatabaseInput(0),
-			Ingestion_preferInput(1),
 			Ingestion_maxThreads(5),
-			Ingestion_compress(1);
+
+			Analyzer_maxThreads(4);
 
 			private int	key;
 
@@ -168,39 +180,54 @@ public class Config {
 
 		}
 
-		public static enum DoubleTypes {
-			PlaceHolder_Dunno(13.37);
+		public static enum BooleanTypes {
+			JUnit_TestValue(true),
+			Ingestion_readXML(true),
+			Ingestion_compress(true);
 
-			private double	key;
+			private boolean	key;
 
-			DoubleTypes(double key) {
+			BooleanTypes(boolean key) {
 				this.key = key;
 			}
 
-			public void setKey(double key) {
+			public void setKey(boolean key) {
 				this.key = key;
 			}
 
-			public double getKey() {
+			public boolean getKey() {
 				return key;
 			}
 
 		}
 
 		public static enum StringTypes {
-			
+
+			Webarchiv_websiteFolder("websites"),
+
+			Queues_SaveLocation("queues"),
+
 			DataBase_RootFolderLocation("." + File.separator + "db"),
-			JUnit_DemoValue("whatsoever"),
-			
-			Compression_MainPath("." + File.separator + "compression" + File.separator),			
+			DataBase_Address("78.46.189.61"),
+			DataBase_Database("webarchiv"),
+			DataBase_Username("webarchiv"),
+			DataBase_Password("t8TPuSZwwDQGtNuG"),
+			JUnit_TestValue("DO_NOT_CHANGE"),
+
 			Plugin_Language("DE; ENG"),
-			Plugin_Domain("DE; COM"),
-			
-			
-			Ingestion_rootDirectory("." + File.separator + "xmlData"),
-			Ingestion_validator("." + File.separator + "Schema.xsd"),
-			Ingestion_crawlerDate("2013-5-30"), //Later set to Today
-			Ingestion_titel("myTitel"); 
+			Plugin_Domain("DE; COM; ORG"),
+
+			Compression_StandartFilename("compressedFile"),
+
+			Ingestion_rootDirectory("XML"),
+			Ingestion_crawlerDate("date"),
+			Ingestion_dateformat("dd-MM-yyyy HH:mm:ss"),
+			Ingestion_url("url"),
+			Ingestion_title("title"),
+			Ingestion_prefix(""),
+			Ingestion_delimiter("/ -"),
+			Ingestion_ratedKeyWordTags("rKWT"),
+			Ingestion_headMetaTags("tag1;tag2;tag3");
 
 			private String	key;
 
---
+++ code/common/QueueHandler.java
@@ -0,0 +1,239 @@
+package common;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import crawler.CrawlingRequest;
+
+import dataStructures.Website;
+
+/**Class for methods that save and load queues
+ * 
+ * @author J. Dietrich
+ *
+ */
+public class QueueHandler {
+	
+	private Logger log;
+	
+	private String				CRAWLERINPUTQUEUE_LOCATION = Config.Keys_Defs.StringTypes.Queues_SaveLocation.getKey() + File.separator + "CrawlerInputQueue.bin";
+	private String				CRAWLEROUTPUTQUEUE_LOCATION = Config.Keys_Defs.StringTypes.Queues_SaveLocation.getKey() + File.separator + "CrawlerOutputQueue.bin";
+	private String				INGESTIONQUEUE_LOCATION = Config.Keys_Defs.StringTypes.Queues_SaveLocation.getKey() + File.separator + "IngestionInputQueue.bin";
+	
+	private BlockingQueue<CrawlingRequest> crawlerInput;
+	private BlockingQueue<Website> crawlerOutput;
+	private BlockingQueue<Website> ingestionInput;
+	
+	/**
+	 * just sets up log
+	 */
+	public QueueHandler() {
+		log = Logger.getLogger(this.getClass().getName());
+		log.setLevel(Level.INFO);
+	}
+
+	
+	/** Method to save the CrawlerOutputQueue
+	 * 
+	 * @param crawlerInputQueue the queue to save
+	 */
+	public void saveCrawlerInputQueue(BlockingQueue<CrawlingRequest> crawlerInputQueue) {
+		if (crawlerInputQueue.size()>0) {
+			File file = new File(CRAWLERINPUTQUEUE_LOCATION);
+			File folder = new File(Config.Keys_Defs.StringTypes.Queues_SaveLocation.getKey());
+			folder.getAbsoluteFile().mkdirs();
+			crawlerInput = new LinkedBlockingQueue<CrawlingRequest>();
+			while(crawlerInputQueue.size()>0) {
+				try {
+					crawlerInput.put(crawlerInputQueue.take());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			OutputStream out;
+			try {
+				out = new FileOutputStream(file);
+				ObjectOutputStream obj_out = new ObjectOutputStream(out);
+				obj_out.writeObject(crawlerInputQueue);
+				obj_out.close();
+				log.info("CrawlerInputQueue saved to file");
+			} catch (FileNotFoundException e) {
+				log.warning("File/Directory was not found");
+			} catch (IOException e) {
+				log.warning("Failed to write to file");
+			}
+		}
+	}
+	
+	/**Method to load the CrawlerOutputQueue
+	 * 
+	 * @return the loaded CrawlerOutputQueue or a new ObservedBlockingQueue if there was nothing saved
+	 */
+	public BlockingQueue<CrawlingRequest> loadCrawlerInputQueue() {
+		BlockingQueue<CrawlingRequest> crawlerInputQueue = new LinkedBlockingQueue<CrawlingRequest>(Config.Keys_Defs.IntTypes.CrawlerSupervisor_CrawlingRequestQueueSize.getKey());;
+		try {
+			File file = new File(CRAWLERINPUTQUEUE_LOCATION);
+			InputStream in = new FileInputStream(file);
+			ObjectInputStream obj_in = new ObjectInputStream(in);
+			crawlerInput = (BlockingQueue<CrawlingRequest>) obj_in.readObject();
+			in.close();
+			if(file.exists()) {
+				file.delete();
+			}
+			log.info("CrawlerInputQueue loaded from file");
+		} catch (Exception e) {
+			crawlerInput = new LinkedBlockingQueue<CrawlingRequest>(Config.Keys_Defs.IntTypes.CrawlerSupervisor_CrawlingRequestQueueSize.getKey());
+			log.info("CrawlerOutputQueue wasn't loaded from file");
+		}
+		putIt put = new putIt(crawlerInput, crawlerInputQueue);
+		put.run();
+		return crawlerInputQueue;
+	}
+	
+	/** Method to save the CrawlerOutputQueue
+	 * 
+	 * @param crawlerOutputQueue the queue to save
+	 */
+	public void saveCrawlerOutputQueue(BlockingQueue<Website> crawlerOutputQueue) {
+		if (crawlerOutputQueue.size()>0) {
+			File file = new File(CRAWLEROUTPUTQUEUE_LOCATION);
+			File folder = new File(Config.Keys_Defs.StringTypes.Queues_SaveLocation.getKey());
+			folder.getAbsoluteFile().mkdirs();
+			crawlerOutput = new LinkedBlockingQueue<Website>();
+			while(crawlerOutputQueue.size()>0) {
+				try {
+					crawlerOutput.put(crawlerOutputQueue.take());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			OutputStream out;
+			try {
+				out = new FileOutputStream(file);
+				ObjectOutputStream obj_out = new ObjectOutputStream(out);
+				obj_out.writeObject(crawlerOutputQueue);
+				obj_out.close();
+				log.info("CrawlerOutputQueue saved to file");
+			} catch (FileNotFoundException e) {
+				log.warning("File/Directory was not found");
+			} catch (IOException e) {
+				log.warning("Failed to write to file");
+			}
+		}
+	}
+	
+	/**Method to load the CrawlerOutputQueue
+	 * 
+	 * @return the loaded CrawlerOutputQueue or a new ObservedBlockingQueue if there was nothing saved
+	 */
+	public BlockingQueue<Website> loadCrawlerOutputQueue() {
+		BlockingQueue<Website> crawlerOutputQueue = new LinkedBlockingQueue<Website>(Config.Keys_Defs.IntTypes.CrawlerOutputQueue_MaxSize.getKey());
+		try {
+			File file = new File(CRAWLEROUTPUTQUEUE_LOCATION);
+			InputStream in = new FileInputStream(file);
+			ObjectInputStream obj_in = new ObjectInputStream(in);
+			crawlerOutput = (LinkedBlockingQueue<Website>) obj_in.readObject();
+			in.close();
+			if(file.exists()) {
+				file.delete();
+			}
+			log.info("CrawlerOutputQueue loaded from file");
+		} catch (Exception e) {
+			crawlerOutput = new LinkedBlockingQueue<Website>(Config.Keys_Defs.IntTypes.CrawlerOutputQueue_MaxSize.getKey());
+			log.info("CrawlerOutputQueue wasn't loaded from file");
+		}
+		putIt put = new putIt(crawlerOutput, crawlerOutputQueue);
+		put.run();
+		return crawlerOutputQueue;
+	}
+	
+	/** Method to save the IngestionInputQueue
+	 * 
+	 * @param ingestionInputQueue the queue to save
+	 */
+	public void saveIngestionInputQueue(BlockingQueue<Website> ingestionInputQueue) {
+		if (ingestionInputQueue.size()>0) {
+			File file = new File(INGESTIONQUEUE_LOCATION);
+			File folder = new File(Config.Keys_Defs.StringTypes.Queues_SaveLocation.getKey());
+			folder.getAbsoluteFile().mkdirs();
+			ingestionInput = new LinkedBlockingQueue<Website>();
+			while(ingestionInputQueue.size()>0) {
+				try {
+					ingestionInput.put(ingestionInputQueue.take());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			OutputStream out;
+			try {
+				out = new FileOutputStream(file);
+				ObjectOutputStream obj_out = new ObjectOutputStream(out);
+				obj_out.writeObject(ingestionInputQueue);
+				obj_out.close();
+				log.info("IngestionInputQueue saved to file");
+			} catch (FileNotFoundException e) {
+				log.warning("File/Directory was not found");
+			} catch (IOException e) {
+				log.warning("Failed to write to file");
+			}
+		}
+	}
+	
+	/**Method to load the IngestionInputQueue
+	 * 
+	 * @return the loaded IngestionInputQueue or a new PriorityBlockingQueue if there was nothing saved
+	 */
+	public BlockingQueue<Website> loadIngestionInputQueue() {
+		BlockingQueue<Website> ingestionInputQueue = new LinkedBlockingQueue<Website>(Config.Keys_Defs.IntTypes.IngestionInputQueue_MaxSize.getKey());
+		try {
+			File file = new File(INGESTIONQUEUE_LOCATION);
+			InputStream in = new FileInputStream(file);
+			ObjectInputStream obj_in = new ObjectInputStream(in);
+			ingestionInputQueue = (LinkedBlockingQueue<Website>) obj_in.readObject();
+			in.close();
+			if(file.exists()) {
+				file.delete();
+			}
+			log.info("IngestionInputQueue loaded from file");
+		} catch (Exception e) {
+			ingestionInputQueue = new LinkedBlockingQueue<Website>(Config.Keys_Defs.IntTypes.IngestionInputQueue_MaxSize.getKey());
+			log.info("IngestionInputQueue wasn't loaded from file");
+		}
+		putIt put = new putIt(ingestionInput, ingestionInputQueue);
+		put.run();
+		return ingestionInputQueue;
+	}
+	
+	class putIt implements Runnable {
+
+		private BlockingQueue localQueue;
+		private BlockingQueue programQueue;
+		
+		public putIt(BlockingQueue localQueue, BlockingQueue programQueue) {
+			this.localQueue = localQueue;
+			this.programQueue = programQueue;
+		}
+		
+		@Override
+		public void run() {
+			while(localQueue.size()>0) {
+				try {
+					programQueue.put(localQueue.take());
+				} catch (InterruptedException e) {
+					log.warning("Can't put object to queue");
+				}
+			}
+		}
+	}
+}
